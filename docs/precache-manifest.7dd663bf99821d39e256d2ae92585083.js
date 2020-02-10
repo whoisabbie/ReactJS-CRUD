dd1118903f7e602020c7d19ec51c95d9d7988cd1
whoisabbie <44428844+whoisabbie@users.noreply.github.com>
2020-02-10 18:15:13 +0530

    Add files via upload

---
+++ docs/precache-manifest.7dd663bf99821d39e256d2ae92585083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efb94c44ad6e9ee9a70099f24df3c08",
+    "url": "/ReactJS-CRUD/index.html"
+  },
+  {
+    "revision": "01c344ee56aec78bf8a0",
+    "url": "/ReactJS-CRUD/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c4625bb57046581435e2",
+    "url": "/ReactJS-CRUD/static/js/2.94b4632c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ReactJS-CRUD/static/js/2.94b4632c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01c344ee56aec78bf8a0",
+    "url": "/ReactJS-CRUD/static/js/main.16a743c0.chunk.js"
+  },
+  {
+    "revision": "8673f4bda79f9fb5806d",
+    "url": "/ReactJS-CRUD/static/js/runtime-main.a40aa98c.js"
+  }
+]);
